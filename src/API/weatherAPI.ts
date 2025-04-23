2fabc373047c29c6e94eb830f2936fe10d009398
= <=>
2025-04-24 00:23:37 +0300

    fix

--- src/API/weatherAPI.ts
+++ src/API/weatherAPI.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { Degree } from '../Types/Degree';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-// const API_KEY = '66da95a586317a9f5385a84ed763ea55';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 const unitSystemMap: Record<Degree, 'metric' | 'imperial'> = {
